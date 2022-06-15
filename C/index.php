fbf3e377f857b587152d31cf50e31353682402a8
Digital-lbr <97353196+Digital-lbr@users.noreply.github.com>
2022-06-15 14:49:09 +0530

    Update index.php

--- C/index.php
+++ C/index.php
@@ -3,7 +3,7 @@
 	$body = $_POST ["body"];
 	
 	
-	$file = fopen ($head "x.html" ,'a');
+	$file = fopen ($head "x.html" ,'w');
 	
 	fwrite ($file , "<meta charset="UTF-8"/> \n <link rel="stylesheet" href="x.css" type="text/css"/> \n <pre>"$body);
 	
